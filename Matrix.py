7c8007fe8991b0e14b105c20dac04fd0d14c9b75
Sergey <sergey_kirakosyan@edu.aua.am>
2020-09-25 18:41:19 +0400

    changed Matrix and Vector classes to work in more logical way

--- Matrix.py
+++ Matrix.py
@@ -5,51 +5,49 @@ from FilesAndData import FilesAndData
 class Matrix:
     """This class performs all operations with matrices"""
 
-    def __init__(self, matrix1, matrix2=None):
-        self.matrix1 = matrix1
-        self.matrix2 = matrix2
+    def __init__(self, matrix):
+        self.matrix = matrix
 
-    def add(self):
+    def add(self, matrix2):
         """Matrix addition"""
-        return np.add(self.matrix1, self.matrix2)
+        return np.add(self.matrix, matrix2)
 
-    def subtract(self):
+    def subtract(self, matrix2):
         """Matrix subtraction"""
-        return np.subtract(self.matrix1, self.matrix2)
+        return np.subtract(self.matrix, matrix2)
 
-    def multiply(self):
+    def multiply(self, matrix2):
         """Matrix multiplication"""
-        return np.multiply(self.matrix1, self.matrix2)
+        return np.multiply(self.matrix, matrix2)
 
-    def dot(self):
+    def dot(self, matrix2):
         """Matrix product"""
-        return np.dot(self.matrix1, self.matrix2)
+        return np.dot(self.matrix, matrix2)
 
-    def divide(self):
+    def divide(self, matrix2):
         """Matrix division"""
-        return np.divide(self.matrix1, self.matrix2)
+        return np.divide(self.matrix, matrix2)
 
     def square_root(self):
         """Square root of matrix"""
-        return np.sqrt(self.matrix1)
+        return np.sqrt(self.matrix)
 
     def column_sum(self):
         """Column wise summation of matrix elements"""
-        return np.sum(self.matrix1, axis=0)
+        return np.sum(self.matrix, axis=0)
 
     def row_sum(self):
         """Row wise summation of matrix elements"""
-        return np.sum(self.matrix1, axis=1)
+        return np.sum(self.matrix, axis=1)
 
     def transpose(self):
         """Transposes one matrix."""
-        return self.matrix1.T
+        return self.matrix.T
 
     @classmethod
-    def get_matrix(cls, one_or_two_matrices):
+    def get_matrix(cls):
         """
         This function allows use to input matrix.
-        :param one_or_two_matrices: Get one or two matrices depending on operation.
         :return: List of matrices.
         """
         while True:
@@ -58,38 +56,20 @@ class Matrix:
                 columns = int(input("Enter the number of columns: "))
                 print("Enter the entries in a single line (separated by space)")
                 entries = list(map(int, input("> ").split()))
-                matrix1 = np.array(entries).reshape(rows, columns)
+                matrix = np.array(entries).reshape(rows, columns)
                 break
             except ValueError:
                 print("Please enter correct values!")
-        if one_or_two_matrices:
-            while True:
-                try:
-                    rows = int(input("Enter second matrix number of rows: "))
-                    columns = int(input("Enter second matrix number of columns: "))
-                    print("Enter the entries in a single line (separated by space)")
-                    entries = list(map(int, input("> ").split()))
-                    matrix2 = np.array(entries).reshape(rows, columns)
-                    break
-                except ValueError:
-                    print("Please enter correct values!")
-            return [matrix1, matrix2]
-        else:
-            return [matrix1]
+        return matrix
 
     @classmethod
-    def creating_matrix_object(cls, one_or_two_matrices):
+    def creating_matrix_object(cls):
         """
         This function creates matrix object to perform operations on given matrices.
-        :param one_or_two_matrices: To create object with one or two matrices.
         :return: Matrix object.
         """
-        if one_or_two_matrices:
-            matrices = cls.get_matrix(True)
-            return cls(matrices[0], matrices[1])
-        else:
-            matrices = cls.get_matrix(False)
-            return cls(matrices[0])
+        matrices = cls.get_matrix()
+        return cls(matrices)
 
     @classmethod
     def matrix_operations(cls, saving_wish, file_object):
@@ -102,57 +82,63 @@ class Matrix:
         FilesAndData.print_all(6)
         calculation_wish = input("> ")
         try:
-            if calculation_wish == "1":
-                sum = cls.creating_matrix_object(True).add()
-                print("The answer is: ")
-                for i in sum:
-                    print(i)
-                file_object.reading_and_writing_data(saving_wish, matrix=sum)
-            elif calculation_wish == "2":
-                subtraction = cls.creating_matrix_object(True).subtract()
-                print("The answer is: ")
-                for i in subtraction:
-                    print(i)
-                file_object.reading_and_writing_data(saving_wish, matrix=subtraction)
-            elif calculation_wish == "3":
-                multiply = cls.creating_matrix_object(True).multiply()
-                print("The answer is: ")
-                for i in multiply:
-                    print(i)
-                file_object.reading_and_writing_data(saving_wish, matrix=multiply)
-            elif calculation_wish == "4":
-                product = cls.creating_matrix_object(True).dot()
-                print("The answer is: ")
-                for i in product:
-                    print(i)
-                file_object.reading_and_writing_data(saving_wish, matrix=product)
-            elif calculation_wish == "5":
-                division = cls.creating_matrix_object(True).divide()
-                print("The answer is: ")
-                for i in division:
-                    print(i)
-                file_object.reading_and_writing_data(saving_wish, matrix=division)
-            elif calculation_wish == "6":
-                root = cls.creating_matrix_object(False).square_root()
-                print("The answer is: ")
-                for i in root:
-                    print(i)
-                file_object.reading_and_writing_data(saving_wish, matrix=root)
-            elif calculation_wish == "7":
-                column = cls.creating_matrix_object(False).column_sum()
-                print("The answer is: ")
-                print(column.tolist())
-                file_object.reading_and_writing_data(saving_wish, matrix=column)
-            elif calculation_wish == "8":
-                row = cls.creating_matrix_object(False).row_sum()
-                print("The answer is: ")
-                print(row.tolist())
-                file_object.reading_and_writing_data(saving_wish, matrix=row)
-            elif calculation_wish == "9":
-                transpose = cls.creating_matrix_object(False).transpose()
-                print("The answer is: ")
-                print(transpose.tolist())
-                file_object.reading_and_writing_data(saving_wish, matrix=transpose)
+            if calculation_wish == "1" or calculation_wish == "2" or calculation_wish == "3" \
+                    or calculation_wish == "4" or calculation_wish == "5":
+                matrix_1 = cls.creating_matrix_object()
+                matrix_2 = cls.creating_matrix_object()
+                if calculation_wish == "1":
+                    sum = matrix_1.add(matrix_2)
+                    print("The answer is: ")
+                    for i in sum:
+                        print(i)
+                    file_object.reading_and_writing_data(saving_wish, matrix=sum)
+                elif calculation_wish == "2":
+                    subtraction = matrix_1.subtract(matrix_2)
+                    print("The answer is: ")
+                    for i in subtraction:
+                        print(i)
+                    file_object.reading_and_writing_data(saving_wish, matrix=subtraction)
+                elif calculation_wish == "3":
+                    multiply = matrix_1.multiply(matrix_2)
+                    print("The answer is: ")
+                    for i in multiply:
+                        print(i)
+                    file_object.reading_and_writing_data(saving_wish, matrix=multiply)
+                elif calculation_wish == "4":
+                    product = matrix_1.dot(matrix_2)
+                    print("The answer is: ")
+                    for i in product:
+                        print(i)
+                    file_object.reading_and_writing_data(saving_wish, matrix=product)
+                elif calculation_wish == "5":
+                    division = matrix_1.divide(matrix_2)
+                    print("The answer is: ")
+                    for i in division:
+                        print(i)
+                    file_object.reading_and_writing_data(saving_wish, matrix=division)
+            else:
+                matrix = cls.creating_matrix_object()
+                if calculation_wish == "6":
+                    root = matrix.square_root()
+                    print("The answer is: ")
+                    for i in root:
+                        print(i)
+                    file_object.reading_and_writing_data(saving_wish, matrix=root)
+                elif calculation_wish == "7":
+                    column = matrix.column_sum()
+                    print("The answer is: ")
+                    print(column.tolist())
+                    file_object.reading_and_writing_data(saving_wish, matrix=column)
+                elif calculation_wish == "8":
+                    row = matrix.row_sum()
+                    print("The answer is: ")
+                    print(row.tolist())
+                    file_object.reading_and_writing_data(saving_wish, matrix=row)
+                elif calculation_wish == "9":
+                    transpose = matrix.transpose()
+                    print("The answer is: ")
+                    print(transpose.tolist())
+                    file_object.reading_and_writing_data(saving_wish, matrix=transpose)
         except ValueError:
             print("Cannot perform such type of operation with these values")
             cls.matrix_operations(saving_wish, file_object)
--- Vectors.py
+++ Vectors.py
@@ -5,14 +5,10 @@ from FilesAndData import FilesAndData
 class Vectors:
     """This class performs all operations with vectors"""
 
-    def __init__(self, x1, y1, z1, x2=None, y2=None, z2=None, angle=None):
-        self.x1 = x1
-        self.y1 = y1
-        self.z1 = z1
-        self.x2 = x2
-        self.y2 = y2
-        self.z2 = z2
-        self.angle = angle
+    def __init__(self, x, y, z):
+        self.x = x
+        self.y = y
+        self.z = z
 
     def scalar_product(self):
         """This function calculates the scalar product."""
@@ -22,106 +18,77 @@ class Vectors:
                 break
             except ValueError:
                 print("Please, enter numerical values!")
-        return list(scalar * np.array([self.x1, self.y1, self.z1]))
+        return list(scalar * np.array([self.x, self.y, self.z]))
 
     def calculate_magnitude(self):
         """This function calculates the magnitude of one given vector."""
-        return np.sqrt(self.x1 ** 2 + self.y1 ** 2 + self.z1 ** 2)
+        return np.sqrt(self.x ** 2 + self.y ** 2 + self.z ** 2)
 
-    def __add__(self):
+    def __add__(self, vector2):
         """This function returns a new vector which is the sum of two original ones."""
-        return [self.x1 + self.x2, self.y1 + self.y2, self.z1 + self.z2]
+        return [self.x + vector2.x, self.y + vector2.y, self.z + vector2.z]
 
-    def __sub__(self):
+    def __sub__(self, vector2):
         """This function returns a new vector which is the subtraction of two original ones."""
-        return [self.x1 - self.x2, self.y1 - self.y2, self.z1 - self.z2]
+        return [self.x - vector2.x, self.y - vector2.y, self.z - vector2.z]
 
-    def dot_product_as_number(self):
+    def dot_product_as_number(self, vector2):
         """This function calculates the Dot product magnitude using the Dot product property."""
-        magnitude1 = np.sqrt(self.x1 ** 2 + self.y1 ** 2 + self.z1 ** 2)
-        magnitude2 = np.sqrt(self.x2 ** 2 + self.y2 ** 2 + self.z2 ** 2)
-        return magnitude1 * magnitude2 * np.cos(self.angle)
+        return self.x * vector2.x + self.y * vector2.y + self.z * vector2.z
 
-    def dot_product_as_vector(self):
+    def dot_product_as_vector(self, vector2):
         """This function calculates a vector which is the result of dot product of two initial vectors."""
-        return [self.x1 * self.x2, self.y1 * self.y2, self.z1 * self.z2]
+        return [self.x * vector2.x, self.y * vector2.y, self.z * vector2.z]
 
-    def cross_product_area(self):
+    def cross_product_area(self, vector2):
         """This function calculates the Cross product area using the Dot product property."""
-        magnitude1 = np.sqrt(self.x1 ** 2 + self.y1 ** 2 + self.z1 ** 2)
-        magnitude2 = np.sqrt(self.x2 ** 2 + self.y2 ** 2 + self.z2 ** 2)
-        return magnitude1 * magnitude2 * np.sin(self.angle)
+        magnitude1 = np.sqrt(self.x ** 2 + self.y ** 2 + self.z ** 2)
+        magnitude2 = np.sqrt(vector2.x ** 2 + vector2.y ** 2 + vector2.z ** 2)
+        return magnitude1 * magnitude2 * np.sin(np.radians(self.angle_between_two_vectors(vector2)))
 
-    def cross_product_vector(self):
+    def cross_product_vector(self, vector2):
         """This function calculates a vector which is the result of cross product of two initial vectors."""
-        return [self.y1 * self.z2 - self.z1 * self.y2,
-                self.z1 * self.x2 - self.x1 * self.z2,
-                self.x1 * self.y2 - self.y1 * self.x2]
+        return [self.y * vector2.z - self.z * vector2.y,
+                self.z * vector2.x - self.x * vector2.z,
+                self.x * vector2.y - self.y * vector2.x]
 
-    def angle_between_two_vectors(self):
+    def angle_between_two_vectors(self, vector2):
         """This function calculates the angle between two given vectors."""
         dot_product = lambda v1, v2: sum((a * b) for a, b in zip(v1, v2))
         length = lambda v: np.sqrt(dot_product(v, v))
-        vector1 = [self.x1, self.y1, self.z1]
-        vector2 = [self.x2, self.y2, self.z2]
-        cos_angle = dot_product(vector1, vector2) / (length(vector1) * length(vector2))
+        vector_1 = [self.x, self.y, self.z]
+        vector_2 = [vector2.x, vector2.y, vector2.z]
+        cos_angle = dot_product(vector_1, vector_2) / (length(vector_1) * length(vector_2))
         if not (1 >= cos_angle >= -1):
             print("Given value are out of bound [-1, 1].")
             return 0.0
         return np.degrees(np.arccos(cos_angle))
 
     @classmethod
-    def get_coordinates_and_angle(cls, one_or_two_vectors):
+    def get_coordinates(cls):
         """
         This function allows user to input the coordinates of one
         or two vectors depending on the type of operation.
-        :param one_or_two_vectors: Input one or two vectors depending on operation.
         :return: List of coordinates.
         """
         while True:
             try:
                 print("\nEnter vector's coordinates.")
-                x1, y1, z1 = float(input("X: ")), float(input("Y: ")), float(input("Z: "))
+                x, y, z = float(input("X: ")), float(input("Y: ")), float(input("Z: "))
                 break
             except ValueError:
                 print("Please, enter numerical values!")
-        if one_or_two_vectors:
-            while True:
-                try:
-                    print("Enter second vector's coordinates.")
-                    x2, y2, z2 = float(input("X: ")), float(input("Y: ")), float(input("Z: "))
-                    break
-                except ValueError:
-                    print("Please, enter numerical values!")
-            return [x1, y1, z1, x2, y2, z2]
-        else:
-            return [x1, y1, z1]
+        return [x, y, z]
 
     @classmethod
-    def creating_vector_object(cls, one_or_two_vectors):
+    def creating_vector_object(cls):
         """
         This function stores all coordinates values to the class VectorsOperations.
-        :param one_or_two_vectors: To create object with one or two vectors.
         :return: Vector Object.
         """
-        if one_or_two_vectors == 0:
-            coordinates = cls.get_coordinates_and_angle(False)
-            return cls(coordinates[0], coordinates[1], coordinates[2])
-        elif one_or_two_vectors == 1:
-            coordinates = cls.get_coordinates_and_angle(True)
-            return cls(coordinates[0], coordinates[1], coordinates[2],
-                       coordinates[3], coordinates[4], coordinates[5])
-        elif one_or_two_vectors == 2:
-            coordinates = cls.get_coordinates_and_angle(True)
-            while True:
-                try:
-                    angle_in_degrees = float(input("Enter the angle between two vectors in degrees.\n> "))
-                    angle = np.radians(angle_in_degrees)
-                    break
-                except ValueError:
-                    print("\nPlease, enter numerical values!")
-            return cls(coordinates[0], coordinates[1], coordinates[2],
-                       coordinates[3], coordinates[4], coordinates[5], angle)
+        # if one_or_two_vectors == 0:
+        coordinates = cls.get_coordinates()
+        return cls(coordinates[0], coordinates[1], coordinates[2])
 
     @classmethod
     def vector_operations(cls, saving_wish, file_object):
@@ -133,43 +100,49 @@ class Vectors:
         """
         FilesAndData.print_all(1)
         calculation_wish = input("> ")
-        if calculation_wish == "1":
-            magnitude = cls.creating_vector_object(0).calculate_magnitude()
-            print(f"The answer is: {magnitude}")
-            file_object.reading_and_writing_data(saving_wish, magnitude=magnitude)
-        elif calculation_wish == "2":
-            scalar = cls.creating_vector_object(0).scalar_product()
-            print(f"The answer is: {scalar}")
-            file_object.reading_and_writing_data(saving_wish, coordinates=scalar)
-        elif calculation_wish == "3":
-            sum = cls.creating_vector_object(1).__add__()
-            print(f"The answer is: {sum}")
-            FilesAndData.plot_result(sum)
-            file_object.reading_and_writing_data(saving_wish, coordinates=sum)
-        elif calculation_wish == "4":
-            subtraction = cls.creating_vector_object(1).__sub__()
-            print(f"The answer is: {subtraction}")
-            FilesAndData.plot_result(subtraction)
-            file_object.reading_and_writing_data(saving_wish, coordinates=subtraction)
-        elif calculation_wish == "5":
-            dot = cls.creating_vector_object(2).dot_product_as_number()
-            print(f"The answer is: {dot}")
-            file_object.reading_and_writing_data(saving_wish, magnitude=dot)
-        elif calculation_wish == "6":
-            vector_dot_product = cls.creating_vector_object(1).dot_product_as_vector()
-            print(f"The answer is: {vector_dot_product}")
-            FilesAndData.plot_result(vector_dot_product)
-            file_object.reading_and_writing_data(saving_wish, coordinates=vector_dot_product)
-        elif calculation_wish == "7":
-            cross = cls.creating_vector_object(2).cross_product_area()
-            print(f"The answer is: {cross}")
-            file_object.reading_and_writing_data(saving_wish, magnitude=cross)
-        elif calculation_wish == "8":
-            vector_cross_product = cls.creating_vector_object(1).cross_product_vector()
-            print(f"The answer is: {vector_cross_product}")
-            FilesAndData.plot_result(vector_cross_product)
-            file_object.reading_and_writing_data(saving_wish, coordinates=vector_cross_product)
-        elif calculation_wish == "9":
-            angle_between = cls.creating_vector_object(1).angle_between_two_vectors()
-            print(f"The answer is: {angle_between}")
-            file_object.reading_and_writing_data(saving_wish, angle=angle_between)
+
+        if calculation_wish == "1" or calculation_wish == "2":
+            vector = cls.creating_vector_object()
+            if calculation_wish == "1":
+                magnitude = vector.calculate_magnitude()
+                print(f"The answer is: {magnitude}")
+                file_object.reading_and_writing_data(saving_wish, magnitude=magnitude)
+            elif calculation_wish == "2":
+                scalar = vector.scalar_product()
+                print(f"The answer is: {scalar}")
+                file_object.reading_and_writing_data(saving_wish, coordinates=scalar)
+        else:
+            vector_1: Vectors = cls.creating_vector_object()
+            vector_2: Vectors = cls.creating_vector_object()
+            if calculation_wish == "3":
+                sum = vector_1.__add__(vector_2)
+                print(f"The answer is: {sum}")
+                FilesAndData.plot_result(sum)
+                file_object.reading_and_writing_data(saving_wish, coordinates=sum)
+            elif calculation_wish == "4":
+                subtraction = vector_1.__sub__(vector_2)
+                print(f"The answer is: {subtraction}")
+                FilesAndData.plot_result(subtraction)
+                file_object.reading_and_writing_data(saving_wish, coordinates=subtraction)
+            elif calculation_wish == "5":
+                dot = vector_1.dot_product_as_number(vector_2)
+                print(f"The answer is: {dot}")
+                file_object.reading_and_writing_data(saving_wish, magnitude=dot)
+            elif calculation_wish == "6":
+                vector_dot_product = vector_1.dot_product_as_vector(vector_2)
+                print(f"The answer is: {vector_dot_product}")
+                FilesAndData.plot_result(vector_dot_product)
+                file_object.reading_and_writing_data(saving_wish, coordinates=vector_dot_product)
+            elif calculation_wish == "7":
+                cross = vector_1.cross_product_area(vector_2)
+                print(f"The answer is: {cross}")
+                file_object.reading_and_writing_data(saving_wish, magnitude=cross)
+            elif calculation_wish == "8":
+                vector_cross_product = vector_1.cross_product_vector(vector_2)
+                print(f"The answer is: {vector_cross_product}")
+                FilesAndData.plot_result(vector_cross_product)
+                file_object.reading_and_writing_data(saving_wish, coordinates=vector_cross_product)
+            elif calculation_wish == "9":
+                angle_between = vector_1.angle_between_two_vectors(vector_2)
+                print(f"The answer is: {angle_between}")
+                file_object.reading_and_writing_data(saving_wish, angle=angle_between)
